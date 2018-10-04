c34aab95522d7162930269f59239fed2fd87b68d
Young Jun <0jun0815@gmail.com>
2018-10-04 15:32:14 +0900

    리팩토링 - TutorialMasterViewController 수정 - TutorialContentsViewController 수정

--- YJWeather/View Controller/Tutorial/TutorialContentsViewController.swift
+++ YJWeather/View Controller/Tutorial/TutorialContentsViewController.swift
@@ -9,25 +9,27 @@
 import UIKit
 
 class TutorialContentsViewController: UIViewController {
+    // MARK: - Properties
+    // MARK: -
+    var pageIndex: Int?
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var descriptionView: UIView!
-    
     @IBOutlet var thermometerImageView: UIImageView!
     @IBOutlet var umbrellaImageView: UIImageView!
     @IBOutlet var humidityImageView: UIImageView!
     @IBOutlet var windImageView: UIImageView!
     @IBOutlet var windDegImageView: UIImageView!
     
-    
-    var pageIndex: Int!
-    var imageFile: String!
-    
+    // MARK: - View lifecycle
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if pageIndex != 2 {
+        guard let pageIndex = pageIndex else {
+            return
+        }
+        if pageIndex < 2 {
             // 전달받은 이미지 정보를 이미지 뷰에 대입한다.
-            imageView.image = UIImage(named: imageFile)
+            imageView.image = UIImage(named: "tutorial\(pageIndex+1)")
             imageView.isHidden = false
             descriptionView.isHidden = true
         } else {
@@ -37,25 +39,20 @@ class TutorialContentsViewController: UIViewController {
         }
     }
     
-    
+    // MARK: - Custom methods
+    // MARK: -
     func initializeDescriptionView() {
         descriptionView.layer.cornerRadius = 10
         view.bringSubview(toFront: descriptionView)
-        
         thermometerImageView.image = thermometerImageView.image?.withRenderingMode(.alwaysTemplate)
         thermometerImageView.tintColor = UIColor.white
-        
         umbrellaImageView.image = umbrellaImageView.image?.withRenderingMode(.alwaysTemplate)
         umbrellaImageView.tintColor = UIColor.white
-        
         humidityImageView.image = humidityImageView.image?.withRenderingMode(.alwaysTemplate)
         humidityImageView.tintColor = UIColor.white
-        
         windImageView.image = windImageView.image?.withRenderingMode(.alwaysTemplate)
         windImageView.tintColor = UIColor.white
-        
         windDegImageView.image = windDegImageView.image?.withRenderingMode(.alwaysTemplate)
         windDegImageView.tintColor = UIColor.white
     }
-    
 }
--- YJWeather/View Controller/Tutorial/TutorialMasterViewController.swift
+++ YJWeather/View Controller/Tutorial/TutorialMasterViewController.swift
@@ -8,131 +8,119 @@
 
 import UIKit
 
-class TutorialMasterViewController: UIViewController, UIPageViewControllerDataSource {
-
-    private var pageViewController: UIPageViewController!
-    private let contentCount = 3
+class TutorialMasterViewController: UIViewController {
+    // MARK: - Properties
+    // MARK: -
+    private let contentsCount = 3
     
+    // MARK: - View lifecycle
+    // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        pageViewController = UIStoryboard(name: "Tutorial", bundle: Bundle.main).instantiateViewController(withIdentifier: "PageVC") as! UIPageViewController
+        guard let pageViewController = UIStoryboard(name: "Tutorial", bundle: Bundle.main).instantiateViewController(withIdentifier: "PageVC") as? UIPageViewController else {
+            return
+        }
         pageViewController.dataSource = self
-        
-        // 페이지 뷰 컨트롤러의 기본 페이지 지정
-        let startContentVC = getContentVC(atIndex: 0)! // 최초 노출될 콘텐츠 뷰 컨트롤러
-        pageViewController.setViewControllers([startContentVC], direction: .forward, animated: true, completion: nil)
-        
-        // 페이지 뷰 컨트롤러의 출력 영역 지정
-        if AppDelegate.isIPhoneX() {
+        // 최초 노출될 콘텐츠 뷰 컨트롤러
+        guard let startContentsVC = getContentsVC(0) else {
+            return
+        }
+        // pageViewController의 기본 페이지 지정
+        pageViewController.setViewControllers([startContentsVC], direction: .forward, animated: true, completion: nil)
+        /* To do */
+        // pageViewController의 출력 영역 지정
+        if UIDevice.currentIPhone == .iPhoneX {
             pageViewController.view.frame.origin = CGPoint(x: 0, y: 120)
             pageViewController.view.frame.size.width = UIScreen.main.bounds.width
             pageViewController.view.frame.size.height = UIScreen.main.bounds.height - 200
         } else {
             let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
-            
             // 전화 상태일 경우
             if statusBarHeight == 40 {
                 pageViewController.view.frame.origin = CGPoint(x: 0, y: 0)
                 pageViewController.view.frame.size.width = UIScreen.main.bounds.width
                 pageViewController.view.frame.size.height = UIScreen.main.bounds.height - 60
-                
             } else {
                 pageViewController.view.frame.origin = CGPoint(x: 0, y: 0)
                 pageViewController.view.frame.size.width = UIScreen.main.bounds.width
                 pageViewController.view.frame.size.height = UIScreen.main.bounds.height - 40
             }
         }
-        
-        
-        // 페이지 뷰 컨트롤러를 마스터 뷰 컨트롤러의 자식 뷰 컨트롤러로 설정
+        /* End to do */
+        // pageViewController TutorialMasterViewController의 자식 뷰 컨트롤러로 설정
         addChildViewController(pageViewController)
         view.addSubview(pageViewController.view)
         pageViewController.didMove(toParentViewController: self)
-        
-        
+        // pageControl 생성
         let pageControl = UIPageControl.appearance()
         pageControl.currentPageIndicatorTintColor = UIColor.black
         pageControl.pageIndicatorTintColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
     }
     
-    private func getContentVC(atIndex idx: Int) -> UIViewController? {
+    // MARK: - Custom methods
+    // MARK: -
+    private func getContentsVC(_ index: Int) -> UIViewController? {
         // 인덱스가 범위를 벗어나면 nil을 반환
-        guard contentCount >= idx && contentCount > 0 else {
+        guard contentsCount >= index, contentsCount > 0 else {
             return nil
         }
-        
-        guard let cvc = UIStoryboard(name: "Tutorial", bundle: Bundle.main).instantiateViewController(withIdentifier: "ContentsVC") as? TutorialContentsViewController else {
+        guard let contentsVC = UIStoryboard(name: "Tutorial", bundle: Bundle.main).instantiateViewController(withIdentifier: "ContentsVC") as? TutorialContentsViewController else {
             return nil
         }
-        
-        cvc.pageIndex = idx
-        
-        if idx == 0 {
-            cvc.imageFile = "tutorial1"
-        } else if idx == 1 {
-            cvc.imageFile = "tutorial2"
-        }
-        
-        return cvc
+        contentsVC.pageIndex = index
+        return contentsVC
     }
     
+    // MARK: - IBAction methods
+    // MARk: -
     @IBAction func close(_ sender: Any) {
         let ud = UserDefaults.standard
-        
+        // 튜토리얼 자동 실행은 최초 1회만
         if ud.bool(forKey: "TUTORIAL") == false {
             ud.set(true, forKey: "TUTORIAL")
             ud.synchronize()
         }
-        
         dismiss(animated: true, completion: nil)
-        //presentingViewController?.dismiss(animated: true, completion: nil)
     }
-    
-    
-    
-    // MARK: PageViewController data source methods
-    
+}
+
+extension TutorialMasterViewController: UIPageViewControllerDataSource {
+    // MARK: - UIPageViewControllerDataSource
+    // MARK: -
     // 현재의 콘텐츠 뷰 컨트롤러보다 앞쪽에 올 콘텐츠 뷰 컨트롤러 객체
     // 즉, 현재의 상태에서 앞쪽으로 스와이프했을 때 보여줄 콘텐츠 뷰 컨트롤러 객체
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
         // 현재 페이지 인덱스
-        guard var index = (viewController as! TutorialContentsViewController).pageIndex else {
+        guard let contentsVC = viewController as? TutorialContentsViewController else {
             return nil
         }
-        
         // 현재 인덱스가 맨 앞이라면 nil을 반환하고 종료
-        guard index > 0 else {
+        guard var index = contentsVC.pageIndex, index > 0 else {
             return nil
         }
-        
-        index -= 1 // 현재의 인덱스에서 하나 뺌(즉, 이전 페이지 인덱스)
-        return self.getContentVC(atIndex: index)
+        // 현재의 인덱스에서 하나 뺌(즉, 이전 페이지 인덱스)
+        index -= 1
+        return getContentsVC(index)
     }
-    
     // 현재의 콘텐츠 뷰 컨트롤러보다 뒤쪽에 올 콘텐츠 뷰 컨트롤러 객체
     // 즉, 현재의 상태에서 뒤쪽으로 스와이프했을 때 보여줄 콘텐츠 뷰 컨트롤러 객체
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         
-        // 현재의 페이지 인덱스
-        guard var index = (viewController as! TutorialContentsViewController).pageIndex else {
+        // 현재 페이지 인덱스
+        guard let contentsVC = viewController as? TutorialContentsViewController else {
             return nil
         }
-        
-        index += 1 // 현재의 인덱스에 하나를 더함(즉, 다음 페이지 인덱스)
-        
         // 인덱스는 항상 배열 데이터의 크기보다 작아야 한다.
-        guard index < contentCount else {
+        guard var index = contentsVC.pageIndex, index + 1 < contentsCount else {
             return nil
         }
-        
-        return self.getContentVC(atIndex: index)
+        // 현재의 인덱스에 하나를 더함(즉, 다음 페이지 인덱스)
+        index += 1
+        return getContentsVC(index)
     }
     
-    
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return contentCount
+        return contentsCount
     }
     
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
